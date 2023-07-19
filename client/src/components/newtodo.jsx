cc23d59c083e540a64f786c5c625ad23e0972e31
BenjaminSchenk <benjamin.k.schenk@gmail.com>
2023-07-19 02:37:32 -0700

    Update newtodo.jsx

--- client/src/components/newtodo.jsx
+++ client/src/components/newtodo.jsx
@@ -3,18 +3,30 @@ import { useEffect, useState } from 'react'
 const Newtodo = () => {
     const [newItem, setNewitem] = useState('')
     const recordResponse = (e) => {
+        e.preventDefault()
         setNewitem(e.target.value)
     }
 
     const submitRespomse = (e) => {
-        
+        const obj = {
+            item: newItem,
+        }
+        e.preventDefault()
+        fetch('https://todo-list-ben.onrender.com/todos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        }); setNewitem('')
+
     }
 
     return (
         <>
         <form>
-        <input id="new"value="" required onChange={(e) => recordResponse(e)}></input>
-        <input id="sub" type="submit" value="submit" onSubmit={(e) => { submitRespomse(e)}}></input>
+        <input id="new"value={newItem} required onChange={(e) => recordResponse(e)}></input>
+        <input id="sub" type="submit" value="submit" onSubmit={(e) => { submitRespomse(e) }}></input>
         </form>
         </>
     )
